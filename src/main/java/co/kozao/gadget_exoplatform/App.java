e30dedd90686e4829c1915cb1ccb8648bf81037d
Carelle9000 <ornellatiako730@gmail.com>
2025-08-01 14:44:42 +0100

    first commit

---
+++ src/main/java/co/kozao/gadget_exoplatform/App.java
@@ -0,0 +1,10 @@
+package co.kozao.gadget_exoplatform;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
